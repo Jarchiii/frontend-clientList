34244f81bbdb393abc69747ccacbec416fe4db5e
Jon <orejajonathan@gmail.com>
2020-06-02 16:51:05 +0200

    a

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node ./src/index.jsx
+web: node ./src/index.js
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-
-    <App />
+    <App/>
     </BrowserRouter>
 ,
   document.getElementById('root')
